2b285d62e3fbea7d6e01f71932f155ed346a76a2
DerikVoid <165839202+DerikVoid@users.noreply.github.com>
2024-06-08 16:42:47 -0300

    Add files via upload

---
+++ Derik e Brunno/HOMI8.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    $nome = "Till Lindesmann";
+
+    if($nome == "Richard Kruspel") {
+        echo "E ae Richard Kruspel";
+    } elseif($nome == "Oliver Riedel") {
+        echo "E ae Oliver Riedel";
+    } elseif($nome == "Till Lindemann") {
+        echo "E ae Till Lindemann";
+    } else {
+    echo "E ae $nome!";
+
+    }
+    ?>
+</body>
+</html>
